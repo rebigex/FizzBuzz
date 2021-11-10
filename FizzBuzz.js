7aa6aaf302d84d53489a0d994a069c0d1a464760
Lior <cool123pep@gmail.com>
2021-11-09 19:15:17 -0500

    enter

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,1 +1,28 @@
-
+function FizzBuzz() {
+    let count = prompt("range: ");
+    for(i=0;i<count;i++) {
+        if(i % 3 == 0) {
+            console.log("Fizz");
+        } else if(i % 5 == 0) {
+            console.log("Buzz");
+        } else if(i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz")
+        } else {
+            console.log(i);
+        }
+        
+    }
+    console.log("FizzBuzz completed")
+}
+while(true) {
+    let playGame = prompt("Play FizzBuzz? y/n: ");
+    if (playGame === "y") {
+        FizzBuzz();
+    } else if (playGame === "n") {
+        console.log("okay, bye");
+        stop(); 
+        break;
+    } else {
+        console.log("invalid input, try again")
+    };
+};
